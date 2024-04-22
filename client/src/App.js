430eb1764057ff192fa8c9750ce91bbf8eaf5976
thushari-uop <thushariherath716@gmail.com>
2024-04-22 12:08:41 +0530

    Routes for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
+import { Navigation } from "@mui/icons-material";
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
+import Dashboard from "scenes/dashboard";
 
 function App() {
 
-  const mode = useSelector((state) => state.global.mode);
-  const theme = useMemo( () => createTheme(themeSettings(mode)), [mode]);
+	const mode = useSelector((state) => state.global.mode);
+	const theme = useMemo( () => createTheme(themeSettings(mode)), [mode]);
 
-  return (
-    <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline/>
-      </ThemeProvider>
-    </div>
-  ); 
+	return (
+		<div className="app">
+			<BrowserRouter>
+				<ThemeProvider theme={theme}>
+					<CssBaseline/>
+          <Routes>
+            <Route element={<Layout/>}>
+              <Route path="/" element={<Navigation to="/dashboard" replace />}/>
+              <Route path="/dashboard" element={<Dashboard/>} />
+            </Route>
+          </Routes>
+				</ThemeProvider>
+			</BrowserRouter>
+		</div>
+	); 
 }
 
 export default App;
- 
+
