2bd061d25562c60c2b9e722903a040cb613e2bf9
thushari-uop <thushariherath716@gmail.com>
2024-04-22 11:53:14 +0530

    setting up matireal ui theams

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+
 function App() {
+
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo( () => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+      </ThemeProvider>
     </div>
-  );
+  ); 
 }
 
 export default App;
+ 
