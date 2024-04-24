b55d8278ffa6bd39e5bffa432db6df45664ec79d
thushari-uop <thushariherath716@gmail.com>
2024-04-24 16:29:16 +0530

    Sidebar open and close

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,17 +6,20 @@ import { GridMenuIcon } from '@mui/x-data-grid';
 import { DarkModeOutlined, LightModeOutlined, Search, SettingsOutlined } from '@mui/icons-material';
 import { setMode } from 'state';
 
-const Navbar = () => {
-
+const Navbar = ({isSidebarOpen, setIsSidebarOpen} ) => {
 	const dispatch = useDispatch();
 	const theme = useTheme();
-
 	return (
-		<AppBar sx={{position: "static", background: "none", boxShadow: "none" }}>
+		<AppBar 
+			sx={{
+			position: "static", 
+			background: "none", 
+			boxShadow: "none" 
+			}}>
 			<Toolbar sx={{justifyContent: "space-between"}}>
 				{/* Left side */}
 			<FlexBetween>
-				<IconButton onClick={() => console.log('open/close sidebar')}>
+				<IconButton onClick = {() => setIsSidebarOpen(!isSidebarOpen)}>
 					<GridMenuIcon/>
 				</IconButton>
 				<FlexBetween backgroundColor={theme.palette.background.alt} borderRadious="9px" gap="3rem" p="0.1rem 1.5rem">
