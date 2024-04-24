e8749336fde909cbd063420b7f52830e01c1d953
thushari-uop <thushariherath716@gmail.com>
2024-04-24 15:37:15 +0530

    Layout

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Box, useMediaQuery } from '@mui/material';
+import Navbar from 'components/Navbar';
+import React, { useState } from 'react'
+import Dashboard from 'scenes/dashboard';
+import Sidebar from 'components/Sidebar';
 
-function Layout() {
-  return (
-    <div>Layout</div>
-  )
+const Layout = () => {
+
+	const isNonMobile = useMediaQuery("(min-width: 600px)");
+	const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+	return (
+		<Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+			<Sidebar 
+				isNonMobile = {isNonMobile} 
+				drawerWidth = "250px" 
+				isSidebarOpen = {isSidebarOpen}
+				setIsSidebarOpen = {setIsSidebarOpen}
+			/>
+				<Box>
+					<Navbar 
+						isSidebarOpen = {isSidebarOpen}
+						setIsSidebarOpen = {setIsSidebarOpen}
+					/>
+					<Dashboard/>
+				</Box>
+		</Box>
+	)
 }
 
-export default Layout
+export default Layout;
