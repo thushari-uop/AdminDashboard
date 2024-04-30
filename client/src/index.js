e9b87a483312dd25536a9cc664e78451460b39d2
thushari-uop <thushariherath716@gmail.com>
2024-04-30 16:30:08 +0530

    settup listner and dispatch

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,17 @@ import App from './App';
 import { configureStore } from '@reduxjs/toolkit';
 import globalReducer from "state";
 import { Provider } from 'react-redux';
+import { api } from 'state/api';
+import { setupListeners } from '@reduxjs/toolkit/query';
 
 const store = configureStore({
 	reducer: {
 		global: globalReducer,
+		[api.reducerPath]: api.reducer,
 	},
+	middleware: (getDefault) => getDefault().concat(api.middleware),
 });
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
