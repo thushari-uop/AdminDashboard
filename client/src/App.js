a431be6cb9cbd0d55c6c4d14546831fb14100a8c
thushari-uop <thushariherath716@gmail.com>
2024-05-06 09:47:30 +0530

    Customer UI and functinality

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
 import Dashboard from "scenes/dashboard";
 import Layout from "scenes/layout";
-import Products from "scenes/products"
+import Products from "scenes/products";
+import Customers from "scenes/customers";
 function App() {
 
 	const mode = useSelector((state) => state.global.mode);
@@ -21,6 +22,7 @@ function App() {
 								<Route path="/" element={<Navigate to="/dashboard" replace />}/>
 								<Route path="/dashboard" element={<Dashboard/>} />
 								<Route path="/products" element={<Products/>} />
+								<Route path="/customers" element={<Customers/>} />
 							</Route>
 						</Routes>
 				</ThemeProvider>
